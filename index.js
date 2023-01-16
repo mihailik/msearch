8bfe93675208700379e293db5dc1674667a4c478
mihailik <mihailik@gmail.com>
2023-01-16 16:27:12 +0000

    Try to hijack 'send'

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-''
+// @ts-check
+
+if (!XMLHttpRequest.prototype.super_send)
+  XMLHttpRequest.prototype.super_send = XMLHttpRequest.prototype.send;
+
+  /** @this {XMLHttpRequest} */
+XMLHttpRequest.prototype.send = function xhrSendOverride(body) {
+  var xhr = this;
+  setTimeout(function () {
+    xhr.super_send(body);
+  }, 2000);
+};
